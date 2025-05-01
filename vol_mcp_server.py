0efa55b61c8a38b92582f50df4f46ec199ae1e17
bakch92 <bakch94@naver.com>
2025-05-02 00:31:38 +0900

    initial_0502

--- vol_mcp_server.py
+++ vol_mcp_server.py
@@ -5,37 +5,37 @@ import subprocess
 mcp = FastMCP("Search")
 
 @mcp.tool()
-def get_processes(dump_image_path: str) -> List[str]:
+def get_processes(image_path: str) -> List[str]:
     """
     Get the processes running in the memory image.
     """
     result = subprocess.run(
-        ['python', '-m' 'vol', '-f', dump_image_path, 'windows.pslist.PsList'],
+        ['vol', '-f', image_path, 'windows.pslist.PsList'],
         capture_output=True,
         text=True
     )
     return result.stdout
 
 @mcp.tool()
-def get_connections(dump_image_path: str) -> List[str]:
+def get_connections(image_path: str) -> List[str]:
     """
     Get the connections running in the memory image.
     """
     result = subprocess.run(
-        ['python', '-m' 'vol', '-f', dump_image_path, 'windows.netscan.NetScan'],
+        ['vol', '-f', image_path, 'windows.netscan.NetScan'],
         capture_output=True,
         text=True
     )
     return result.stdout
 
 @mcp.tool()
-def get_cmdline(dump_image_path: str) -> List[str]:
+def get_cmdline(image_path: str) -> List[str]:
     """
     Get the command line arguments of the processes in the memory image.
     """
 
     result = subprocess.run(
-        ['python', '-m' 'vol', '-f', dump_image_path, 'windows.cmdline.CmdLine'],
+        ['vol', '-f', image_path, 'windows.cmdline.CmdLine'],
         capture_output=True,
         text=True
     )
